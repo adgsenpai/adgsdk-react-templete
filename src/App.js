1b200e16bd3aa1234d6142f2089bcce715b13c95
adgsenpai <adgrules@hotmail.com>
2022-05-06 00:05:51 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import "./static/css/nucleo-svg.css"
+import "./static/css/nucleo-icons.css"
+import "./static/css/nucleo-svg.css"
+import "./static/css/soft-ui-dashboard.css?v=1.0.3"  
+import "./static/js/soft-ui-dashboard.js"
+ 
+function App() {
+  return (
+    <div id="OutputDOM">
+    </div>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+import axios from 'axios';
+
+ 
